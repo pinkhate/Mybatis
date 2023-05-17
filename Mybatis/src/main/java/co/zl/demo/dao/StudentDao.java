d59e5621ddde1fa6181e1c2395cdd92e353e48a1
pinkhate <2471588682@qq.com>
2023-05-17 19:27:47 +0800

    mybatis

---
+++ Mybatis/src/main/java/co/zl/demo/dao/StudentDao.java
@@ -0,0 +1,8 @@
+package co.zl.demo.dao;
+
+/**
+ * @author ZL
+ * @creatTime 2023-05-17
+ */
+public class StudentDao {
+}
---
+++ Mybatis/src/main/java/co/zl/demo/entity/Student.java
@@ -0,0 +1,16 @@
+package co.zl.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @author ZL
+ * @creatTime 2023-05-17
+ */
+@Data
+public class Student {
+
+    private Integer sno;
+    private String name;
+    private Integer age;
+    private String major;
+}
